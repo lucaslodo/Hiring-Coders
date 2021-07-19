932b7784408d385c7c0aa2b67f6d72785a010a69
Lucas Lodo <lucasbap87@gmail.com>
2021-07-19 00:59:53 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -7,4 +7,38 @@ emailButton.addEventListener('click', (e) => {
 
 function saveData(email) {
     console.log(email);
+    
+    if (validateEmail(email)) {
+        let data = JSON.parse(localStorage.getItem('email-save'));
+        if (data != null) {
+            let used = data.filter((savedEmails) => {return savedEmails === email});
+            if (used != null && used.length > 0) {
+                resultFromSubmit(2);
+                return false;
+            } else {
+                data.push(email);
+                console.log(data);
+            }
+        } else {
+            data = [email];
+        }
+        localStorage.setItem('email-save', JSON.stringify(data));
+        resultFromSubmit(1);
+    } else {
+        resultFromSubmit(0);
+    }
+}
+function resultFromSubmit(value) {
+    let msg = document.getElementById('return');
+    if (value == 1) {
+        msg.textContent = 'E-mail Cadastrado com sucesso';
+    } else {
+        msg.textContent = 'E-mail inválido! Tente Novamente!';
+    }
+    
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
